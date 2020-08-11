c1fe4cc1638eccef90d6d2fdb3f980bdac2004b0
ovsyankafood <49985415+ovsyankafood@users.noreply.github.com>
2020-08-11 22:05:20 +0300

    Добавили автообновление меню для детокса и низкоугл

--- app/containers/HomePage/components/ProductDescription.js
+++ app/containers/HomePage/components/ProductDescription.js
@@ -3,11 +3,52 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import _ from 'lodash';
 
-import molodostMenuNU from '../../../files/menu-iun-2020-antistress-1300.jpg';
-import detoxMenuNU from '../../../files/menu-iun-2020-antistress-detox.jpg';
+import healthLowCarbMenuNU from '../../../files/menu-iun-2020-zdorovie-1300.jpg';
+import youthLowCarbMenuNU from '../../../files/menu-iun-2020-antistress-1300.jpg';
+import beautyLowCarbMenuNU from '../../../files/menu-iun-2020-molodost-1300.jpg';
+
+import healthDetoxMenuNU from '../../../files/menu-iun-2020-zdorovie-detox.jpg';
+import youthDetoxMenuNU from '../../../files/menu-iun-2020-antistress-detox.jpg';
+import beautyDetoxMenuNU from '../../../files/menu-iun-2020-molodost-detox.jpg';
+
 import CloseIcon from '../../../components/Header/img/close.svg';
+
+const lowCarbMenu = {
+  1: healthLowCarbMenuNU,
+  2: beautyLowCarbMenuNU,
+  3: youthLowCarbMenuNU,
+};
+const detoxMenu = {
+  1: healthDetoxMenuNU,
+  2: beautyDetoxMenuNU,
+  3: youthDetoxMenuNU,
+};
+const getWeekNumber = (w, menu) => {
+  let week;
+  if (w > 3) {
+    week = w % 3;
+  } else {
+    week = w;
+  }
+
+  if (_.isInteger(week / 3)) {
+    return menu[3];
+  }
+  if (_.isInteger(week / 2)) {
+    return menu[2];
+  }
+  return menu[1];
+};
+
+const startPoint = moment([2019, 4, 6]);
+const daysDiff = moment().diff(startPoint, 'days');
+const week = Math.floor(daysDiff / 7) + 1;
+
+const currentLowCarbMenuNU = getWeekNumber(week, lowCarbMenu);
+const currentDetoxMenuNU = getWeekNumber(week, detoxMenu);
 
 export class ProductDescription extends React.PureComponent {
   constructor(props) {
@@ -86,14 +127,14 @@ export class ProductDescription extends React.PureComponent {
               )}
               {el.menuHref && (
                 <div>
-                  <a target="_blank" href={molodostMenuNU}>
+                  <a target="_blank" href={currentLowCarbMenuNU}>
                     Посмотреть меню программы
                   </a>
                 </div>
               )}
               {el.menuDetoxHrev && (
                 <div>
-                  <a target="_blank" href={detoxMenuNU}>
+                  <a target="_blank" href={currentDetoxMenuNU}>
                     Посмотреть меню программы
                   </a>
                 </div>
